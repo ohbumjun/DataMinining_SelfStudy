d8f7ddb0322cbe6b961a0d2fed35a7c6c996140a
OH BUM JUN <beomjun0638@hanmail.net>
2021-09-07 12:42:26 +0900

    apply 함수

--- DataFrame3.py
+++ DataFrame3.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import os
 import pandas as pd
@@ -62,3 +63,117 @@ abalon_df
 abalon_df.groupby(['sex', 'length_bool']).mean()
 
 abalon_df.groupby(['sex', 'length_bool'])['whole_weight'].mean()
+
+# -------------------------------------------------------
+# 중복 데이터 확인하기
+# 중복데이터 삭제 결측치 --> 중복된 데이터 확인하기
+
+# 중복된 row 확인하기
+abalon_df.duplicated()
+abalon_df.duplicated().sum()  # 0 !
+
+# 중복 예제 생성을 위해 --> 가상으로 중복데이터 생성
+# pd.concat() : 데이터 붙이기
+new_abalon = abalon_df.iloc[[0]]  # [[]] : data frame 형태로 가져오기
+
+# abalon_df, new_abalone 을, 가로로 붙일 것이다 ( 아래 row에 붙인다 )
+new_abalon_df = pd.concat([abalon_df, new_abalon], axis=0)
+
+new_abalon_df.duplicated()  # 마지막 행 중복
+new_abalon_df.duplicated().sum()  # 1
+
+# 첫번째 중복 데이터 중에서, 처음애를 지우자
+new_abalon_df.duplicated(keep='last')  # 마지막 애를 보존하자
+
+# 중복 데이터 중에서, 첫번째 제외, 그 이후 중복애들 row 삭제
+new_abalon_df = new_abalon_df.drop_duplicates()
+# new_abalon_df = new_abalon_df.drop_duplicates(keep='last') : 마찬가지로 뒤에꺼 죽이기
+
+# NaN(결측치)를 찾아서, 다른 값으로 변경하기
+# 기존 데이터에는 결측치가 존재 x
+abalon_df.isnull().sum()
+
+# 가상으로 결측치 만들기
+nan_abalon_df = abalon_df.copy()
+# 값 변경
+nan_abalon_df.loc[2, 'length'] = np.nan
+nan_abalon_df.isnull().sum()
+
+# 결측치를 특정 값으로 채우기
+
+# 결측치를 0으로 채운다
+zero_abalone_df = nan_abalon_df.fillna(0)
+zero_abalone_df  # 실수로 바뀜 ( 우선순위 : 실수 > 정수 )
+
+# 결측치를 결측치가 속한 컬럼의 평균값으로 대체하기
+nan_abalon_df.mean()
+nan_abalon_df = nan_abalon_df.fillna(nan_abalon_df.mean())
+
+# 아래와 같이 특정 column 및 행렬에 대한 작업도 가능하다
+nan_abalon_df['length'].fillna(nan_abalon_df['length'].mean())
+
+# 카테고리 변수 결측치 처리
+nan_abalon_df.loc[2, 'sex'] = np.nan
+# 평균은 적용할 수 없으므로, 가장 많은 값으로 채우겠다
+nan_abalon_df['sex'].value_counts().max()
+# 혹은
+sex_mode = nan_abalon_df['sex'].value_counts(
+).sort_values(ascending=False).index[0]
+nan_abalon_df['sex'].fillna(sex_mode)
+
+
+# 꽃 ...!
+# apply 함수 !!!! 진짜 강추
+# 본인이 원하는 행과 열에 연산 혹은 function을 적용할 수 있다
+# 열 기준으로 집계하고 싶은 경우 axis = 0
+# 행 기준으로 집계하고 싶은 경우 axis = 1
+
+# 1) 열 기준 집계
+abalon_df[['diameter']].apply(np.average, axis=0)
+
+# 2) 행 기준 집계
+abalon_df[['diameter']].apply(np.average, axis=1)
+
+
+# 사용자 정의함수를 통한 apply
+def avg_ceil(x, y, z):
+    return math.ceil((x+y+z)/3)  # ceil : 올림
+
+
+abalon_df[['diameter', 'height', 'whole_weight']]
+.apply(lambda x: avg_ceil(x[0], x[1], x[2]), axis=1)
+
+# 문제
+
+
+def is_sum_over_3(x, y, z):
+    return (x+y+z) > 1
+
+
+# ['diameter','height','whole_weight'] 변수 사용
+# 세 변수의 합이 1이 넘으면 true, 아니면 false 출력 후 answer 변수에 저장
+# abalon_df 에 answer 열을 추가하고 입력
+abalon_df['answer'] = np.where(
+    abalon_df[['diameter', 'height', 'whole_weight']]
+    .apply(lambda x: is_sum_over_3(x[0], x[1], x[2]), axis=1),
+    True,
+    False)
+abalon_df
+
+
+# ---------------------------------------------------
+# 카테고리 변수
+# 컬럼내 유니크한 값 뽑아서 개수 확인하기 (카테고리 변수에 적용)
+abalon_df['sex'].value_counts(ascending=True)
+# abalon_df['sex'].value_counts(dropna = True) : 결측치 제외하고 보기
+
+
+# --------------------------------------------------
+# 2개의 data frame 합치기
+# 가상 abalon 1개 row 데이터 생성 및 결합
+one_abalon_df = abalon_df.iloc[[0]]
+pd.concat([abalon_df, one_abalon_df], axis=0)
+
+# 가상 abalon 1개 column 데이터 생성 및 결합
+one_abalon_df = abalon_df.iloc[:, [0]]  # 성별 정보
+pd.concat([abalon_df, one_abalon_df], axis=1)
