e0f34c740c1e775a63b0afd3c9dea475a14c33f0
OH BUM JUN <beomjun0638@hanmail.net>
2021-09-24 21:33:55 +0900

    Svm Non-Linear

--- POSTECH_ML/7_SupportVectorMachine.py
+++ POSTECH_ML/7_SupportVectorMachine.py
@@ -360,3 +360,96 @@ plt.xlim([0, 8])
 plt.ylim([-4, 3])
 plt.show()
 # 같은 결과가 나오는 것을 확인할 수 있다
+
+
+# 비선형
+# Non-Linear SVM
+# kernel을 통해서 mapping
+# mapping된 공간 상에서 linear classification을 한 다음
+# 원래 공간상에서 non-linear classification을
+# 얻게 되는 것이다
+X1 = np.array([
+    [-1.1, 0], [-0.3, 0.1], [-0.9, 1],
+    [0.8, 0.4], [0.4, 0.9], [0.3, -0.6],
+    [-0.5, 0.3], [-0.8, 0.6], [-0.5, -0.5]
+])
+
+X0 = np.array([[-1, -1.3], [-1.6, 2.2], [0.9, -0.7],
+               [1.6, 0.5], [1.8, -1.1], [1.6, 1.6],
+               [-1.6, -1.7], [-1.4, 1.8], [1.6, -0.9],
+               [0, -1.6], [0.3, 1.7], [-1.6, 0], [-2.1, 0.2]])
+
+X1 = np.asmatrix(X1)
+X0 = np.asmatrix(X0)
+
+plt.figure(figsize=(10, 8))
+plt.plot(X1[:, 0], X1[:, 1], 'ro', alpha=0.4, label='C!')
+plt.plot(X0[:, 0], X0[:, 1], 'bo', alpha=0.4, label='C0')
+plt.title('SVM for Nonlinear Data', fontsize=15)
+plt.xlabel(r'$x_1$', fontsize=15)
+plt.ylabel(r'$x_2$', fontsize=15)
+plt.legend(loc=1, fontsize=12)
+plt.axis('equal')
+plt.show()
+
+# Kernel 준비하기
+N = X1.shape[0]
+M = X0.shape[0]
+
+X = np.vstack([X1, X0])
+y = np.vstack([np.ones([N, 1]), -np.ones([M, 1])])
+
+X = np.asmatrix(X)
+y = np.asmatrix(y)
+
+m = N + M
+# kernel function --> 열 벡터 형태로 나열하는 것이다
+Z = np.hstack([np.ones([m, 1]), np.square(X[:, 0]), np.sqrt(
+    2)*np.multiply(X[:, 0], X[:, 1]), np.square(X[:, 1])])
+
+g = 10
+
+w = cvx.Variable([4, 1])
+d = cvx.Variable([m, 1])
+
+obj = cvx.Minimize(cvx.norm(w, 2) + g*np.ones([1, m])*d)
+const = [cvx.multiply(y, Z*w) >= 1-d, d >= 0]
+prob = cvx.Problem(obj, const).solve()
+
+w = w.value
+print(w)
+
+# 자. 우리는 mapping을 통해서 4차원으로 mapping 시켰다
+# 그래프상 x,y 축 모두를 -3에서 3까지 0.1 간격으로 meshgrid를 뽑아낸다
+# 해당하는 모든 점들이 x가 될 것이다
+# numpy의 meshgrid함수는 1차원 좌표배열(x1,x2...xn) 에서 n차원
+# 직사각형 격자를 만드는 함수이다.
+[X1gr, X2gr] = np.meshgrid(np.arange(-3, 3, 0.1), np.arange(-3, 3, 0.1))
+Xp = np.hstack([X1gr.reshape(-1, 1), X2gr.reshape(-1, 1)])
+Xp = np.asmatrix(Xp)
+
+m = Xp.shape[0]
+Zp = np.hstack([np.ones([m, 1]), np.square(Xp[:, 0]), np.sqrt(
+    2)*np.multiply(Xp[:, 0], Xp[:, 1]), np.square(Xp[:, 1])])
+# 4차원 공간 상에서의 hyper plane
+q = Zp*w
+
+# q가 0보다 큰 것을 B에 담아둘 것이다
+# B안에 있는 것만 색상으로 grid 표시를 하면
+B = []
+for i in range(m):
+    if q[i, 0] > 0:  # C1 에 해당하는 것
+        B.append(Xp[i, :])
+
+B = np.vstack(B)
+
+plt.figure(figsize=(10, 8))
+plt.plot(X1[:, 0], X1[:, 1], 'ro', label='C!')
+plt.plot(X0[:, 0], X0[:, 1], 'bo', label='C0')
+plt.plot(B[:, 0], B[:, 1], 'gs', markersize=10, alpha=0.1, label='SVM')
+plt.title('SVM for Kernel', fontsize=15)
+plt.xlabel(r'$x_1$', fontsize=15)
+plt.ylabel(r'$x_2$', fontsize=15)
+plt.legend(loc=1, fontsize=12)
+plt.axis('equal')
+plt.show()
