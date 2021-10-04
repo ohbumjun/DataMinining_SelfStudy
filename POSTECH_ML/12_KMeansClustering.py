5d03ca33e5b105e90b52cbcb53158f1759367fc6
OH BUM JUN <beomjun0638@hanmail.net>
2021-10-04 10:01:10 +0900

    K-Means Clustering practice

--- POSTECH_ML/12_KMeansClustering.py
+++ POSTECH_ML/12_KMeansClustering.py
@@ -1,6 +1,10 @@
 # unsupervised learing
 # x 데이터만 존재, partition 개수만 주어져야 한다
 
+from mpl_toolkits.mplot3d import Axes3D
+import pandas as pd
+import os
+import cv2
 from sklearn.cluster import KMeans
 import numpy as np
 import matplotlib.pyplot as plt
@@ -118,4 +122,93 @@ plt.stem(range(1, 11), cost)
 plt.xticks(np.arange(11))
 plt.xlim([0.5, 10.5])
 plt.grid(alpha=0.3)
+plt.show()
+
+# 예제 --------------------------------------------
+# Color Clustering
+
+# '.' : 현재 어떤 파일들이 있는지를 확인하기
+# './drive ' : drive 파일로
+os.listdir('./drive/MyDrive/machine_learning')
+# 데이터 폴더 src 변수 할당
+base_src = './drive/MyDrive/machine_learning'
+# 이미지 파일 src 변수 할당
+img_src = base_src + '/images/bird.bmp'
+
+img = cv2.imread(img_src)
+print(img.shape)
+
+plt.figure(figsize=(10, 8))
+plt.imshow(img)
+plt.axis('off')
+plt.show()
+
+img_gray = cv2.imread(img_src, 0)
+
+plt.figure(figsize=(10, 8))
+plt.imshow(img_gray)
+plt.axis('off')
+plt.show()
+
+# threshold를 잡아서 0 혹은 255  --> 흑백 binary 로
+idx = np.where(img_gray > 2**7)
+print(idx)
+
+threshold_img = np.zeros(img_gray.shape)  # 일단 모두 0으로
+
+threshold_img[idx] = 255
+plt.figure(figsize=(10, 8))
+plt.imshow(threshold_img)
+plt.axis('off')
+plt.show()
+
+# 3차원 공간 상 좌표계 관점으로 접근하기
+k = 2
+m = 538
+print(img.shape)
+X = img.reshape(m*m, 3)  # 3열을 지닌, m*m 개의 행, 형태로
+
+fig = plt.figure(figsize=(10, 8))
+ax = fig.add_subplot(111, projection='3d')
+ax.set_xlabel('$R$', fontsize=15)
+ax.set_ylabel('$G$', fontsize=15)
+ax.set_zlabel('$B$', fontsize=15)
+ax.scatter(X[:, 0], X[:, 1], X[:, 2], marker='.', label='RGB')
+plt.legend(fontsize=15)
+plt.show()
+
+# kmeans를 통해 2개로 강제로 나누겠다
+
+kmeans = KMeans(n_clusters=2, random_state=0)
+kmeans.fit(X)
+
+
+fig = plt.figure(figsize=(10, 8))
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(X[kmeans.labels_ == 0][:, 0],
+           X[kmeans.labels_ == 0][:, 1],
+           X[kmeans.labels_ == 0][:, 2],
+           marker='.',
+           color='r',
+           label='C0')
+ax.scatter(X[kmeans.labels_ == 1][:, 0],  # 1로 labeling 된 것에서의 1번째
+           X[kmeans.labels_ == 1][:, 1],
+           X[kmeans.labels_ == 1][:, 2],
+           marker='.',
+           color='b',
+           label='C1')
+plt.legend(fontsize=15)
+plt.show()
+
+# 이제 0또는 255 로 나눌 것이다
+col_ref = np.array([[255, 255, 255], [0, 0, 0]])
+
+img_bw = np.zeros([m*m, 3])
+
+for i in range(2):
+    img_bw[kmeans.labels_ == i] = col_ref[i, :]
+
+plt.figure(figsize=(10, 8))
+plt.imshow(img_bw.reshape(m, m, 3))
+plt.axis('off')
 plt.show()
